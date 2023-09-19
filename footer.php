8e67fc8d9510e391f095cdca2ed41d424f5f6661
Ahmad051984 <ahmad1984@mail.ru>
2023-09-19 07:41:49 +0300

    created header and footer files

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+			<div class="container">
+				<div class="footer__white">
+					<div class="footer__logo">
+						<div class="footer__logo-item footer__logo-item-main-logo">
+							<a href="#">
+								<img src="img/Logo.png" alt="">
+							</a>
+						</div>
+						<div class="footer__logo-item">
+							<img src="img/CD_Projekt_logo1.svg" alt="">
+						</div>
+						
+					</div>
+					<div class="footer__links">
+						<a href=""><div>Лицензия</div></a>
+						<a href=""><div>Политика конфиденциальности</div></a>
+					</div>
+				</div>
+				</div>
+			<div class="footer__black">CD PROJEKT®, Cyberpunk®, Cyberpunk 2077®</div>
+			
+			
+		</footer>
+
+		<script src="js/swiper-bundle.min.js"></script>
+		<script src="js/script.js"></script>	
+	</div>
+
+
+
+
+
+
+
+</body>
---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<title>mycuber</title>
+	<link rel="stylesheet" href="css/swiper-bundle.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+	<div class="wrapper">
+		<header class="header">
+			<div class="header__main">
+				<div class="container">
+					<div class="header__row">
+						<div class="header__logo">
+							<a href="#">
+								<img src="img/Logo.png" alt="Logo">
+							</a>
+						</div>
+						<ul class="header__icons">
+							<li class="header__items">
+								<a href=""><img src="img/icons/youtube-logo.svg" alt="youtube-logo"></a>
+							</li>
+							<li class="header__items">
+								<a href=""><img src="img/icons/vk-logo.svg" alt="vk-logo"></a>
+							</li>
+							<li class="header__items">
+								<a href=""><img src="img/icons/facebook-logo.svg" alt="facebook-logo"></a>
+							</li>
+							<li class="header__items">
+								<a href=""><img src="img/icons/twitter-logo.svg" alt="twitter-logo"></a>
+							</li>
+							<li class="header__items">
+								<a href=""><img src="img/icons/twitch-logo.svg" alt="twitch-logo"></a>
+							</li>
+							<li class="header__items">
+								<a href=""><img src="img/icons/instagram-logo.svg" alt="instagram-logo"></a>
+							</li>
+						</ul>
+					</div>
+					<div class="header__sticker-row">
+						<div class="header__sticker" style="background-image: url(img/Rectangle\ 59.svg);">
+							<div class="header__title title">Доступно на всех платформах</div>
+							<button class="header__btn btn">
+								<span>Узнать больше</span>
+							</button>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="swiper mySwiper">
+				<div class="swiper-wrapper">
+				  <div class="swiper-slide">
+					<img src="img/Slider1920/s1.jpg" alt="">
+				  </div>
+				  <div class="swiper-slide">
+					<img src="img/Slider1920/s2.jpg" alt="">
+				  </div>
+				  <div class="swiper-slide">
+					<img src="img/Slider1920/s3.jpg" alt="">
+				  </div>
+				</div>
+			</div>
+			<div class="swiper-button-next"></div>
+			<div class="swiper-button-prev"></div>
+		</header>
